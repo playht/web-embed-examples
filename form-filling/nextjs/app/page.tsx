818ffcf91307aed1546cfb4cc27939c94699c499
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-08-05 18:20:11 -0700

    finished change text example

--- form-filling/nextjs/app/page.tsx
+++ form-filling/nextjs/app/page.tsx
@@ -1,20 +1,57 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const webEmbedSrc = "https://cdn.jsdelivr.net/npm/play-ai-embed";
-const webEmbedId = "cqtpW3R1taG6kn7PZM2IB";
+// const webEmbedSrc = "https://cdn.jsdelivr.net/npm/play-ai-embed";
+const webEmbedSrc = "http://localhost:4304/index.js";
+const webEmbedId = "VQkN5p1ijeXxXeGrjx6ZL";
+
+/*
+ * [Agent Greeting]
+ * Hello what do you want to change the text on the screen to?
+ *
+ * [Agent Prompt]
+ * Your only job is to change the text on the page to a given string.
+ * Do not do anything else. Do not offer to do anything else. After changing the text, END THE CALL IMMEDIATELY.
+ */
 
 export default function Home() {
+  const [text, setText] = useState("Change this text");
+
+  // Define your events here
+  const events = [
+    {
+      name: "change-text",
+      description:
+        "This function is called with a string representing the text to change to",
+      parameters: {
+        text: { type: "string", description: "The text to change to" },
+      },
+    },
+  ];
+
+  // Define your event handler here
+  const onEvent = (e: any) => {
+    const event = JSON.parse(e?.payload ?? "{}");
+    if (event.name === "change-text") {
+      setText(event.parameters.text);
+    }
+    console.log("EVENT: ", event);
+  };
+
   useEffect(() => {
-    // window?.PlayAI.open(webEmbedId, { events, onEvent });
-    window?.PlayAI.open(webEmbedId);
+    window?.PlayAI.open(webEmbedId, { events, onEvent });
+    // window?.PlayAI.open(webEmbedId);
   }, [() => window]);
 
   return (
     <>
       {/* Include play web embed library */}
       <script type="text/javascript" src={webEmbedSrc} async />
+
+      <div className="flex justify-center items-center h-[70vh]">
+        <div className="font-medium text-2xl">{text}</div>
+      </div>
     </>
   );
 }
