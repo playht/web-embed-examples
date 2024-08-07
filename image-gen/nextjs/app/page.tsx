34ff7425966264bad1e7b4280090fd8c0f0389d1
oliver-wb <103220077+oliver-wb@users.noreply.github.com>
2024-08-07 12:47:08 -0700

    adds image-gen commenting

--- image-gen/nextjs/app/page.tsx
+++ image-gen/nextjs/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import * as fal from "@fal-ai/serverless-client";
 
+// configure the fal proxy, see the full documentation here: https://fal.ai/docs/integrations/nextjs
 fal.config({
   proxyUrl: "/api/fal/proxy",
 });
@@ -11,48 +12,43 @@ interface FalResult {
   images?: { url: string }[];
 }
 
+/*
+ * global variable necessary for embed event handlers to access updated image as react's useState hook
+ * does not point to the same object in memory when updating a state variable
+ */
+let image_url = ''
+
 export default function Home() {
-  const [image, setImage] = useState<string>("https://fal.media/files/koala/Chls9L2ZnvuipUTEwlnJC.png")
+  const [image, setImage] = useState<string | null>(null)
 
   async function callFalAPI(modelName: string, input: { prompt: string; image_url?: string }) {
-    const result = await fal.subscribe(modelName, {
-      input,
-      logs: true,
-      onQueueUpdate: (update) => {
-        if (update.status === "IN_PROGRESS") {
-          update?.logs?.map((log) => log.message).forEach(console.log);
-        }
-      },
-    }) as FalResult;
-  
+    const result = await fal.subscribe(modelName, { input }) as FalResult;
     if (result?.images?.length && result?.images?.length > 0) {
+      // store the image in state to update the image in the UI
       setImage(result.images[0].url);
+      // store the image in a global variable to access in the embed event handlers for subsequent modifications
+      image_url = result.images[0].url;
     }
   }
   
   async function generateImage(prompt: string) {
+    // call the fal api to generate an image
+    // we've chosen flux/schnell as it is very fast, but feel free to choose any text to image model
     await callFalAPI("fal-ai/flux/schnell", { prompt });
   }
   
   async function modifyImage(prompt: string, previousImage: string) {
-    await callFalAPI("fal-ai/flux/dev", { prompt, image_url: previousImage });
+    // call the fal api to modify the image
+    // we've chosen flux/dev/image-to-image as it is very fast, but feel free to choose any image to image model
+    await callFalAPI("fal-ai/flux/dev/image-to-image", { prompt, image_url: previousImage });
   }
 
-  const onEvent = async (ev: any) => {
-    // event returns a stringified object with name and parameters
-    const { name, parameters } = JSON.parse(ev?.payload ?? '{}');
-    const { userPrompt } = parameters;
-    if (!userPrompt) return;
-    switch (name) {
-      case 'generate-image':
-        await generateImage(userPrompt);
-        break;
-      case 'modify-image':
-        await modifyImage(userPrompt, image);
-        break;
-    }
-  };
-
+/* 
+  This is the list of events that the agent can trigger.
+  It is used to define the functions in the Play AI embed.
+  These events allow the agent to interact with the image generation
+  and modification capabilities of our application. 
+*/
   const events = [
     {
       name: 'generate-image',
@@ -69,6 +65,24 @@ export default function Home() {
       },
     }
   ];
+  // this is the event handler for the embed
+  // it is called whenever the agent sends an event to the embed
+  const onEvent = async (event: any) => {
+    console.log("event called", event)
+    // event returns a stringified object with name and parameters
+    const { name, parameters } = event
+    const { userPrompt } = parameters;
+    if (!userPrompt) return;
+    switch (name) {
+      case 'generate-image':
+        await generateImage(userPrompt);
+        break;
+      case 'modify-image':
+        console.log('previous image', image_url)
+        await modifyImage(userPrompt, image_url);
+        break;
+    }
+  };
 
   // const scriptUrl = "https://cdn.jsdelivr.net/npm/play-ai-embed"
   const scriptUrl = "http://localhost:4304/index.js"
@@ -76,16 +90,17 @@ export default function Home() {
 
   const windowObj = typeof window !== 'undefined' ? window : undefined;
   
+  // open the Play AI embed
   useEffect(() => {
     // @ts-ignore
     windowObj?.PlayAI?.open(webEmbedId, { events, onEvent })
   }, [windowObj])
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center gap p-24">
       <script type="text/javascript" src={scriptUrl} async></script>
-     <Image src={image} alt="Generated Image" width={500} height={500} />
-     <button className="bg-blue-500 text-white p-2 rounded-md" onClick={async () => generateImage('a brown dog')}>Test Fal</button>
+      <Image src={image ?? "/playcube.svg"} alt="Generated Image" width={image ? 500 : 300} height={image ? 500 : 300} />
+     {/* <button className="bg-blue-500 text-white p-2 rounded-md" onClick={async () => generateImage('a brown dog')}>Test Fal API</button> */}
     </main>
   );
 }
